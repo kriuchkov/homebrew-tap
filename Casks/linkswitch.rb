46a00940c09e0693ef692ff2443b25cb67fe3156
Nikita Kriuchkov <nkriuchkov@outlook.com>
2025-12-04 13:01:48 +0300

    Update linkswitch to 1.0.4

--- Casks/linkswitch.rb
+++ Casks/linkswitch.rb
@@ -1,6 +1,6 @@
 cask "linkswitch" do
-  version "1.0.2"
-  sha256 "8d4f8a8bb83b9ed48b4e7a8710731c776ff2ad9bb8f8d4bead9b14caf7e656b0"
+  version "1.0.4"
+  sha256 "7b02b9571e5edc3ebdf0b518363535a7eaadccc053471db87a7f44c8e53f2104"
 
   url "https://github.com/kriuchkov/linkswitch/releases/download/v#{version}/LinkSwitch.zip"
   name "LinkSwitch"
